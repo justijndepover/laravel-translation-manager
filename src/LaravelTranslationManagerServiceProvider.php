4a8bb155a0a33483b43cb17457cbf0c6d27dbe21
Justijn Depover <justijndepover@gmail.com>
2021-05-21 17:32:00 +0200

    wip

--- src/LaravelTranslationManagerServiceProvider.php
+++ src/LaravelTranslationManagerServiceProvider.php
@@ -1,2 +1,21 @@
 <?php
 
+namespace Justijndepover\LaravelTranslationManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class TranslationManagerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Commands\TranslationAddCommand::class,
+            ]);
+        }
+    }
+
+    public function boot()
+    {
+    }
+}
